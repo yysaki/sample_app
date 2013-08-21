ad53ab6b7b88dd23c60b5ecb5df2f8fa2b4f8b00
y_sasaki <vagrant@precise32.(none)>
2013-08-21 06:21:58 +0000

    4.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,13 +9,23 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}");
+    end
+
+    # it "should not have a custom page title" do
+    #   visit '/static_pages/home'
+    #   expect(page).not_to have_title("| Home");
+    # end
+
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       expect(page).to have_title("#{base_title} | Home");
     end
   end
 
-  describe "Home page" do
+  describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
