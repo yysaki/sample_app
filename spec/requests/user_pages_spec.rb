cdd93ff2bb9026db3cfe6aee7601253473e21579
y_sasaki <vagrant@precise32.(none)>
2013-08-23 07:30:23 +0000

    9.1.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,6 +59,23 @@ describe "User pages" do
       it { should have_content('Password is too short (minimum is 6 characters)') }
     end
   end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+  end
 end
 
 #describe "UserPages" do
