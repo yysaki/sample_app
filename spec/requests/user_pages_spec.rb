72fc540324895940b7a3c9d4833ae6e56b2cf867
y_sasaki <vagrant@precise32.(none)>
2013-08-23 05:10:59 +0000

    7.6 演習

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,23 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_title('Sign up') }
+      it { should have_content('error') }
+      it { should have_content('Password can\'t be blank') }
+      it { should have_content('Password is too short (minimum is 6 characters)') }
     end
   end
 end
